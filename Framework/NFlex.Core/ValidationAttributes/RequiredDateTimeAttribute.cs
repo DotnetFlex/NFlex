ed994b26e4b2f592c0ec8d41432a8d1557a861d1
Nick <5340060@qq.com>
2018-01-16 17:49:41 +0800

    增加自定义验证

---
+++ Framework/NFlex.Core/ValidationAttributes/RequiredDateTimeAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFlex.Core.ValidationAttributes
+{
+    public class RequiredDateTimeAttribute : ValidationAttribute
+    {
+        public RequiredDateTimeAttribute()
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var guid = value.To<DateTime>();
+            if (guid == DateTime.MinValue)
+                return new ValidationResult(ErrorMessage);
+            else
+                return ValidationResult.Success;
+        }
+    }
+}
---
+++ Framework/NFlex.Core/ValidationAttributes/RequiredGuidAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NFlex.Core.ValidationAttributes
+{
+    public class RequiredGuidAttribute : ValidationAttribute
+    {
+        public RequiredGuidAttribute()
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var guid = value.To<Guid>();
+            if (guid == Guid.Empty)
+                return new ValidationResult(ErrorMessage);
+            else
+                return ValidationResult.Success;
+        }
+    }
+}
