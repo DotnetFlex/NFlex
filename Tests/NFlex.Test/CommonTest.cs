b0d4638fa1e8a9f0f541635f479d761670c3da12
Nick <5340060@qq.com>
2018-04-12 09:43:14 +0800

    a

--- Tests/NFlex.Test/CommonTest.cs
+++ Tests/NFlex.Test/CommonTest.cs
@@ -17,7 +17,7 @@ namespace NFlex.Test
         [Fact]
         public void RsaEncrypt()
         {
-            string str = "ljzlingc.223";
+            string str = "123";
             string pbk = "9a39c3fefeadf3d194850ef3a1d707dfa7bec0609a60bfcc7fe4ce2c615908b9599c8911e800aff684f804413324dc6d9f982f437e95ad60327d221a00a2575324263477e4f6a15e3b56a315e0434266e092b2dd5a496d109cb15875256c73a2f0237c5332de28388693c643c8764f137e28e8220437f05b7659f58c4df94685";
             string exponent = "010001";
             string result = Encrypt(str, pbk, exponent);
